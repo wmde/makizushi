00810e18a8adc0acfefa455c8ddac9cf0d103bf3
Adam Wight <adam.wight@wikimedia.de>
2022-08-17 10:42:05 +0200

    Numerals match original size
    Also simplifies the stroke behind by using "paint-order".

--- markers-src/alphanum/build.js
+++ markers-src/alphanum/build.js
@@ -1,50 +1,49 @@
 // Requires the fonts-open-sans OS package.
 
 const {spawn} = require('child_process')
+
 const {createSVGWindow} = require('svgdom')
 const {SVG, registerWindow} = require('@svgdotjs/svg.js')
-const window = createSVGWindow()
-registerWindow(window, window.document)
+const beautify = require('xml-formatter')
 
 const sizes = [11, 15, 20]
 
+function createNewCanvas() {
+	const window = createSVGWindow()
+	const document = window.document
+	registerWindow(window, document)
+	const canvas = SVG(document.documentElement)
+	return canvas
+}
+
 function renderTextToSvg(text) {
-	const baseFontSize = 92;
-	const baseFontStyle = {
-		family: 'Open Sans',
-		weight: 'bold',
-		color: '#000000',
-		fill: '#444444',
-		'size': '108px',
-		'text-anchor': 'middle',
-		'text-align': 'center',
-		'dominant-baseline': 'middle',
-	}
-	const doc = SVG(window.document.documentElement).size(100, 100)
-	const group = doc.group()
-	group
-		.text(text)
-		.x(50).y(60)
+	const canvas = createNewCanvas().size(100, 100)
+	const group = canvas.group()
+
+	// debugging
+	// canvas.css({
+		// 'background-color': 'blue',
+		// 'border': '1px solid black'
+	// })
+
+	group.text(text)
+		.x(50).y(70)
 		.font({
-			...baseFontStyle,
-			size: `${baseFontSize * 1.18}px`,
-			//stroke: '#ffffff',
+			size: '108px',
+			family: 'Open Sans',
+			weight: 'bold',
+			fill: '#444444',
+			'size': '108px',
+			'text-anchor': 'middle',
+			'text-align': 'center',
+			stroke: '#ffffff',
 			'stroke-width': '8',
-			stroke: '#ff0000',
-			//'stroke-opacity': '0.3',
-			// 'stroke-linecap': 'butt',
-			// 'stroke-linejoin': 'miter',
-			// 'stroke-miterlimit': '8',
-		})
-	group
-		.text(text)
-		.x(50).y(60)
-		.font({
-			...baseFontStyle,
-			size: `${baseFontSize}px`,
+			'stroke-opacity': '0.3',
+			'fill-opacity': '1',
+			'paint-order': 'stroke',
 		})
 
-	return doc.svg()
+	return beautify(canvas.svg())
 }
 
 function renderSvgToPng(svg, outputPath, size) {
@@ -55,6 +54,7 @@ function renderSvgToPng(svg, outputPath, size) {
 
 function renderAllPngs(text) {
 	const svg = renderTextToSvg(text)
+
 	sizes.forEach(size => {
 		renderSvgToPng(svg, `${text}-${size}.png`, size)
 		renderSvgToPng(svg, `${text}-${size}@2x.png`, size * 2)
@@ -63,5 +63,8 @@ function renderAllPngs(text) {
 
 renderAllPngs("1")
 
-const fs = require('fs')
-fs.writeFileSync("o.svg", renderTextToSvg("1"))
+// debugging
+// const debugSvg = renderTextToSvg("1")
+// console.log(debugSvg)
+// const fs = require('fs')
+// fs.writeFileSync("o.svg", debugSvg)
